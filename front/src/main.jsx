6a46aa2899e9257b495185a935a99b9496921f7b
KharkovskyiVladyslav <vlad.website33@gmail.com>
2025-12-15 14:27:11 +0100

    Frontend-routing setup, layout, home page basics

--- front/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
-);
